42dc1cdda0cafb52750834f1ceb0e13db2b526c0
unknown <asacha@delasacha93297.sapient.com>
2014-11-24 13:28:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,142 @@
+var app = angular.module('myApp', []);
+
+
+
+app.run(function($rootScope, Facebook) {
+
+  $rootScope.Facebook = Facebook;
+
+})
+
+
+
+
+app.factory('Facebook', function() {
+
+    var self = this;
+    this.auth = null;
+    this.userInfo = null;
+    this.friendlist = null;
+
+    return {
+
+      getAuth: function() {
+        return self.auth;
+        
+      },
+      getUserData: function() {
+      	return self.userInfo;
+      },
+      login: function() {
+        $(".btn").hide();
+        FB.login(function(response) {
+          if (response.authResponse) {
+            self.auth = response.authResponse;
+            
+            FB.api(
+	    "/me/picture",
+	    function (response) {
+        var str = response.data.url;
+      // var res = str.replace("v/t1.0-1/c0.0.50.50/p50x50", "");
+
+
+	      if (response && !response.error) {
+	      	        document.getElementById('profilePic').innerHTML = '<img src="'+str+'">';
+	      }
+	    }
+	);
+	
+	FB.api(
+	    "/me",
+	    function (response) {
+	      if (response && !response.error) {
+	      	        document.getElementById('basicInfo').innerHTML = "Name: "+response.name+ "<br/>Gender:" +response.gender;
+	      }
+	    }
+	);
+	
+	            
+
+            FB.api('/me/friendlists', function(response1) {
+            	
+	       	$.each( response1.data, function( key, value ) {
+			console.log( key + ": " + value.id );
+			FB.api(value.id+"/members", function(response2) {
+				$.extend(self.friendlist, response2);
+				$.each( response2.data, function( key, friends ) {
+					console.log( friends.name);
+            $("body").append("<li style='list-style:decimal' class='friendlist'>" +friends.name +"</li>");
+				});
+			});
+		});
+		self.userInfo = response1;
+	        return response1;
+	    });
+          } else {
+            console.log('Facebook login failed', response);
+          }
+        },{scope: 'read_friendlists'});
+
+      },
+       // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+   testAPI: function() {
+    console.log('Welcome!  Fetching your information.... ');
+    
+  },
+
+      logout: function() {
+
+        FB.logout(function(response) {
+          if (response) {
+            self.auth = null;
+          } else {
+            console.log('Facebook logout failed.', response);
+          }
+
+        })
+
+      },
+      loginCheck : function(){
+
+        FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+          // the user is logged in and has authenticated your
+          // app, and response.authResponse supplies
+          // the user's ID, a valid access token, a signed
+          // request, and the time the access token 
+          // and signed request each expire
+          alert("logged In");
+          var uid = response.authResponse.userID;
+          var accessToken = response.authResponse.accessToken;
+        } else if (response.status === 'not_authorized') {
+          // the user is logged in to Facebook, 
+          // but has not authenticated your app
+          alert("not_authorized");
+        } else {
+          // the user isn't logged in to Facebook.
+          alert("not logged in");
+        }
+ });
+
+      }
+
+    }
+
+  })
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: '530294353650337',
+    version    : 'v1.0',
+  });
+};
+
+// Load the SDK Asynchronously
+(function(d){
+    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    ref.parentNode.insertBefore(js, ref);
+}(document));
